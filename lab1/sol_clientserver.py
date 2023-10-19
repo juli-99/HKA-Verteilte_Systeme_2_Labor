e9ced0c4288bf2cf36458780034b2163e474c380
Philip Mader <116314501+pmmader@users.noreply.github.com>
2023-10-19 15:20:30 +0200

    Update sol_clientserver.py get/get_call via call

--- lab1/sol_clientserver.py
+++ lab1/sol_clientserver.py
@@ -43,18 +43,18 @@ class Server:
                         break  # stop if client stopped
                     decoded_data = data.decode('utf-8')
                     msg_out = ''
-                    if decoded_data.startswith('GET:'): #CLient request one entry: GET:name
+                    if decoded_data.startswith('GET:'): # Client request one entry: GET:name
                         self._logger.info(f'GET {decoded_data[4:]} requested via {str(self.sock)}')
-                        try:  #send number if it exists otherwise send unknown key
+                        try:  # send number if it exists otherwise send unknown key
                             msg_out = str(self._data[decoded_data[4:]])
                             self._logger.info('Sent 1 entry')
                         except:
                             msg_out = 'unknown key'
                             self._logger.info('Unknown key')
-                    elif decoded_data.__eq__('GET_ALL'):  #Client requests all entries: GET_ALL
+                    elif decoded_data.__eq__('GET_ALL'):  # client requests all entries: GET_ALL
                         self._logger.info(f'GET ALL requested via {str(self.sock)}')
-                        connection.send(str(len(self._data)).encode('utf-8'))  #send number of entries
-                        if connection.recv(1024).decode('utf-8').__eq__('OK'):  #if client confirms send all entries
+                        connection.send(str(len(self._data)).encode('utf-8'))  # send number of entries
+                        if connection.recv(1024).decode('utf-8').__eq__('OK'):  # if client confirms send all entries
                             for d in self._data:
                                 msg_out += f'{d} : {self._data[d]}\n'
                             self._logger.info(f'Sent {len(self._data)} entries')
@@ -86,37 +86,27 @@ class Client:
         self.logger.info('Client created')
         self.logger.info("Client connected to socket " + str(self.sock))
 
-    def __call(self, msg_in):
+    def __call(self, msg_in, buffer_size=1024):
         """ Call server """
         self.sock.send(msg_in.encode('utf-8'))  # send encoded string as data
         self.logger.info(f'Call {msg_in} via {str(self.sock)}')
-        data = self.sock.recv(1024)  # receive the response
+        data = self.sock.recv(buffer_size)  # receive the response
         msg_out = data.decode('utf-8')
         print(msg_out)  # print the result
         return msg_out
 
     def get(self, name):
         """retrieve number of the given name"""
-        self.sock.send(f'GET:{name}'.encode('utf-8'))  # send GET:name to the server
-        self.logger.info(f'Call GET:{name} via {str(self.sock)}')
-        data = self.sock.recv(1024)  # receive the number
-        self.logger.info('Received 1 entry')
-        msg_out = data.decode('utf-8')
-        print(msg_out)
-        return msg_out
-
+        return self.__call(f'GET:{name}')
 
     def get_all(self):
-        """retrieve all numbers"""
-        self.sock.send('GET_ALL'.encode('utf-8'))  # send GET_ALL to the server
-        self.logger.info(f'Call GET_ALL via {str(self.sock)}')
-        len = int(self.sock.recv(1024).decode('utf-8'))  # receive number of entries
-        self.sock.send('OK'.encode('utf-8')) # send confirmation
-        data = self.sock.recv(32 * len) # receive all entries
-        self.logger.info(f'Received {len} entries')
-        msg_out = data.decode('utf-8') 
-        print(msg_out)
-        return msg_out
+        """ retrieve all numbers """
+        data = self.__call('GET_ALL')
+        if data:
+            num_of_entries = int(data)
+            return self.__call('OK', 32 * num_of_entries)
+        else:
+            return self.__call('Error')
 
     def close(self):
         """ Close socket """
